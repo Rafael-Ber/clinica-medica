218bb3f0ea1e95c039fc524398b23f6ad44991d5
Rafael-Ber <rafaelhenrique1051@gmail.com>
2024-07-03 12:42:46 -0300

    testado e aprovado

--- clinica-medica/clinica-agendamento/src/main/java/br/edu/imepac/repositories/HorarioAtendimentoRepository.java
+++
@@ -1,9 +0,0 @@
-package br.edu.imepac.repositories;
-
-import br.edu.imepac.models.HorarioAtendimento;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HorarioAtendimentoRepository extends JpaRepository<HorarioAtendimento, Long> {
-
